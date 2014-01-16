443bbcc561cd8db7f350ebb2bd698823b0817aaa
saurabh arora <sarora@xebia.com>
2014-01-16 16:25:53 +0530

    @saurabh kapil--made functions in ScoreDao and CourseDao

--- project1_2/src/main/java/com/dao/CourseDao.java
+++ project1_2/src/main/java/com/dao/CourseDao.java
@@ -1,12 +1,46 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.model.Course;
+import com.mysql.jdbc.PreparedStatement;
+import com.util.DbUtil;
+
 public class CourseDao {
 
-	public void getAllCourse() {
+	private Connection connection;
+	private PreparedStatement preparedStatement;
+	private java.sql.ResultSet rs;
+
+	public CourseDao() {
+		connection = DbUtil.getConnection();
+	}
+
+	public List<Course> getAllCourse() throws SQLException {
+
+		List<Course> courses = new ArrayList<Course>();
+		preparedStatement = (PreparedStatement) connection
+				.prepareStatement("select * from course");
+		rs = preparedStatement.executeQuery();
+		while (rs.next()) {
+			Course course = new Course();
+			course.setCourseName(rs.getString("coursename"));
+			course.setCourseId(rs.getInt("courseid"));
+			courses.add(course);
+		}
+		return courses;
 
 	}
 
-	public void addAllCourse() {
+	public void addCourse(Course course) throws SQLException {
+
+		preparedStatement = (PreparedStatement) connection
+				.prepareStatement("insert into course (coursename) values (?)");
+		preparedStatement.setString(1, course.getCourseName());
+		preparedStatement.executeUpdate();
 
 	}
 
--- project1_2/src/main/java/com/dao/ScoreDao.java
+++ project1_2/src/main/java/com/dao/ScoreDao.java
@@ -1,11 +1,51 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.mysql.jdbc.PreparedStatement;
+import com.util.DbUtil;
+
 public class ScoreDao {
-	public void getScore() {
+
+	private Connection connection;
+	private PreparedStatement preparedStatement;
+	private java.sql.ResultSet rs;
+
+	public ScoreDao() {
+		connection = DbUtil.getConnection();
+	}
+
+	public Map<Integer, Double> getScore(int subjectId) throws SQLException {
+
+		Map<Integer, Double> map = new HashMap<Integer, Double>();
+
+		preparedStatement = (PreparedStatement) connection
+				.prepareStatement("select studentid,score from mapstudentscore where subjectid=?");
+		preparedStatement.setInt(1, subjectId);
+
+		rs = preparedStatement.executeQuery();
+		while (rs.next()) {
+
+			map.put(rs.getInt("studentid"), rs.getDouble("score"));
+
+		}
+		return map;
 
 	}
 
-	public void addScore() {
+	public void addScore(int studentId, int subjectId, double score)
+			throws SQLException {
+
+		preparedStatement = (PreparedStatement) connection
+				.prepareStatement("insert into mapstudentscore(studentid,subjectid,score) values (?,?,?)");
+		preparedStatement.setInt(1, studentId);
+		preparedStatement.setInt(2, subjectId);
+		preparedStatement.setDouble(1, score);
+
+		preparedStatement.executeUpdate();
 
 	}
 }
